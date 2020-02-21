8392b4d51cecfe4297e9247d1799cd305f35d2e5
Rangga Putra Raharja <rangga-pr@pop-os.localdomain>
2020-02-21 14:19:10 +0700

    add markdown support

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,14 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
